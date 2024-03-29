726e09bdaa2b67b6f0b9a09795d2960c2e2c2f27
Lubos Zima <lubos.zima@gmail.com>
2024-03-29 15:02:03 +0700

    feat: moved scripts to scripts directory

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 MY_IP=$(terragrunt output --terragrunt-working-dir ./terragrunt/ai/server ipv4 | xargs)
+echo "webui: http://localhost:8080"
 ssh -N -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -L 8080:127.0.0.1:8080  "devops@$MY_IP"
