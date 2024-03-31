b2632018f1f4d088c523b997c447617a9953f37d
Lubos Zima <lubos.zima@gmail.com>
2024-03-31 14:29:39 +0700

    feat: wip

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -2,10 +2,4 @@
 
 MY_IP=$(terragrunt output --terragrunt-working-dir ./terragrunt/ai/server ipv4 | xargs)
 ssh-keygen -R "$MY_IP"
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "devops@$MY_IP"
-
-
-# cycle through a list of items
-echo "llama2-uncensored, llama2:13b" | sed 's/,/\n/g' | xargs -I {} echo "{}" | while read -r item; do
-    echo "item: $item"
-done
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null "devops@$MY_IP"  2>/dev/null
